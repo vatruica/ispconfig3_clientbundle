c9101a4fa3d1ea7718ce782208b84e97da5a5c2c
art3 <art3@anonhq>
2015-10-06 10:58:33 +0200

    client add function included ; $_POST vars now populate the defined vars

--- executeBundleCreation.php
+++ executeBundleCreation.php
@@ -1,6 +1,7 @@
 <?php
-/* authenticate with soap */
-require 'soap_config.php';
+/* show some erros */
+error_reporting(E_ALL);
+ini_set( "display_errors", 1);
 
 /* Generate random password */
 function randomPassword($max) {
@@ -15,33 +16,136 @@ function randomPassword($max) {
 	return implode($pass); //turn the array into a string
 }
 
+/* instantiate SOAP class */
+require 'soap_config.php';
+
 $client = new SoapClient(null, array('location' => $soap_location,
 		'uri'      => $soap_uri,
 		'trace' => 1,
 		'exceptions' => 1));
 
 /* Define user and domain vars */
-$newDomain = 'test.com';
-$newDb = str_replace(array('.'), '' , $newDomain);
-$newUsername = 'tester'; 
-$password_email = randomPassword(12);
+$clientCompany = $_POST["clientcompany"]; 
+$clientName = $_POST["clientname"];
+$clientCity= "Odense";
+$clientCountry = "DK";
+$clientPhone = $_POST["clientphone"];
+$clientEmail = $_POST["clientemail"];
+$clientUsername = str_replace(array(' '), '' , $clientName);
+$clientPassword = randomPassword(12);
+
+$newDomain = $_POST["domainname"];
+$newDb = str_replace(array('.'), '' , $newDomain); 
+$newUsername = $_POST["domainusername"];
+$password_email = randomPassword(12); 
 $password_db = randomPassword(12); 
 $password_ftp = randomPassword(12); 
 $quota_hdd = '1000';
 $quota_traffic = '1000';
-$quota_mail = '500000000'; # 500000000 thats actually 500 mb ; dont know why
-$client_id = 2; # modify with either the default admin user "0" or the one of your choice
+$quota_mail = '500000000'; # 500000000 thats actually 500 mb ; dont know why ; check db table
 
+$today = date("Y-m-d");
+
+
+/* connect to ISPconfig and get session id */
 try {
 	if($session_id = $client->login($soapUsername, $soapPassword)) {
 		echo 'Logged successfull. Session ID:'.$session_id.'<br />';
 	}
 	
+	
+/*
+ * client_add
+ * 
+ * db : dbispconfig -> client
+ * 
+ * 
+ */
+	
+	$reseller_id = 0; // this id has to be 0 if the client shall not be assigned to admin or if the client is a reseller
+	$paramsClientAdd = array(
+			'company_name' => $clientCompany,
+			'contact_name' => $clientName,
+			'customer_no' => '', # fix - just blank for the moment
+			'vat_id' => '',
+			'street' => '',
+			'zip' => '',
+			'city' => $clientCity,
+			'state' => '',
+			'country' => $clientCountry,
+			'telephone' => $clientPhone,
+			'mobile' => $clientPhone,
+			'fax' => '',
+			'email' => $clientEmail,
+			'internet' => '',
+			'icq' => '',
+			'notes' => '',
+			'default_mailserver' => 1,
+			'limit_maildomain' => 1,
+			'limit_mailbox' => 5,
+			'limit_mailalias' => 1,
+			'limit_mailaliasdomain' => 1,
+			'limit_mailforward' => 1,
+			'limit_mailcatchall' => 1,
+			'limit_mailrouting' => 0,
+			'limit_mailfilter' => 5,
+			'limit_fetchmail' => -1,
+			'limit_mailquota' => 500,
+			'limit_spamfilter_wblist' => 0,
+			'limit_spamfilter_user' => 0,
+			'limit_spamfilter_policy' => 1,
+			'default_webserver' => 1,
+			'limit_web_ip' => '',
+			'limit_web_domain' => 1,
+			'limit_web_quota' => 1000,
+			'web_php_options' => 'no,fast-cgi,cgi,mod,suphp,php-fpm',
+			'limit_web_subdomain' => 1,
+			'limit_web_aliasdomain' => 1,
+			'limit_ftp_user' => 2,
+			'limit_shell_user' => 0,
+			'ssh_chroot' => 'no,jailkit,ssh-chroot',
+			'limit_webdav_user' => 0,
+			'default_dnsserver' => 1,
+			'limit_dns_zone' => 0,
+			'limit_dns_slave_zone' => 0,
+			'limit_dns_record' => 0,
+			'default_dbserver' => 1,
+			'limit_database' => 1,
+			'limit_cron' => 0,
+			'limit_cron_type' => 'url',
+			'limit_cron_frequency' => 5,
+			'limit_traffic_quota' => 1000,
+			'limit_client' => 0, // If this value is > 0, then the client is a reseller
+			'parent_client_id' => 0,
+			'username' => $clientUsername,
+			'password' => $clientPassword,
+			'language' => 'en',
+			'usertheme' => 'default',
+			'template_master' => 0,
+			'template_additional' => '',
+			#'created_at' => 0
+			'added_date' => $today
+			);
+	
+	$clientID = $client->client_add($session_id, $reseller_id, $paramsClientAdd);
+	
+	// attributing the client id to the newly created
+	$client_id = $clientID;
+	
 /* sites_web_domain_add 
  * 
  * db : dbispconfig -> web_domain 
- * 
+
+the following are filled like below and not as defined by apache ; look into templates 
+pm_max_children 1
+pm_start_servers 1
+pm_min_spare_Servers 1
+pm_max_spare_servers 1
+pm_process_idle_timeout 1
+pm_max_Requests 10
+
  * */
+	
 	
 	//* Set the function parameters.
 	$paramsWebDomainAdd = array(
@@ -49,7 +153,7 @@ try {
 			'ip_address' => '*',
 			'domain' => $newDomain,
 			'type' => 'vhost',
-			'parent_domain_id' => 0, 
+			'parent_domain_id' => 0, #fix $lastDomainID = "query in db for that" ; parent_domain_id = $lastDomainID + 1 
 			'vhost_type' => 'name',
 			'hd_quota' => $quota_hdd,
 			'traffic_quota' => $quota_traffic,
@@ -59,7 +163,7 @@ try {
 			'errordocs' => 1,
 			'is_subdomainwww' => 1,
 			'subdomain' => '',
-			'php' => 'fast-cgi', # fast-cgi , php-fpm or whatever php module you want ; examples initially show "y" as option, but it will appear as disabled
+			'php' => 'y', #fix - its being registered as "disabled" ; instead of y add string of option : fast-cgi , php-fpm etc
 			'ruby' => 'n',
 			'redirect_type' => '',
 			'redirect_path' => '',
@@ -87,6 +191,7 @@ try {
 			'backup_copies' => 1,
 			'active' => 'y',
 			'traffic_quota_lock' => 'n',
+			'added_date' => $today,
 			'added_by' => $soapUsername
 			# added_date in table is registered as 0000-00-00
 	);
@@ -150,6 +255,8 @@ try {
  * 
  * db : dbispconfig -> web_database
  * 
+ * does not associate with website - website_id has changed into parent_domain_id
+ * atributes default user the first one created, but does not show in options - user is now created first and added to params
  * 
  * */
 
@@ -157,9 +264,9 @@ try {
 	$paramsDBAdd = array(
 			'server_id' => 1,
 			'type' => 'mysql',
-			'parent_domain_id' => $newDomainID, 
+			'parent_domain_id' => $newDomainID, #fix
 			'database_name' => $newDb, # The database name may contain these characters: a-z, A-Z, 0-9 and the underscore. Length: 2 - 64 characters.
-			'database_user_id' => $newDBUserID, 
+			'database_user_id' => $newDBUserID, #fix
 			'database_ro_user_id' => '0',
 			'database_charset' => 'UTF8',
 			'remote_access' => 'n',
@@ -206,10 +313,10 @@ try {
 			'login' => $newUsername . '@' . $newDomain,
 			'password' => $password_email,
 			'name' => $newUsername,
-			'uid' => 5000, 
-			'gid' => 5000, 
+			'uid' => 5000, #fix
+			'gid' => 5000, #fix
 			'maildir' => '/var/vmail/' . $newDomain . '/' . $newUsername,
-			'quota' => $quota_mail, 
+			'quota' => $quota_mail, # 500 ends up into - 0.00047683715820312 ; its a bigint(20)
 			'cc' => '',
 			'homedir' => '/var/vmail',
 			'autoresponder' => 'n',
@@ -236,34 +343,29 @@ try {
 	}
 	
 	/* show output to the user and eventually send mail with it */
-	
-	echo "Domain - " . $newDomain;
-	echo "Can be visited at http://" . $newDomain;
+	echo "</br></br>";
+	echo "Client created with credentials : </br>";
+	echo "Username " . $clientUsername . "</br>";
+	echo "Password " . $clientPassword . "</br>";
+	echo "ID : ".$clientID."<br>";
+	echo "</br>";
+	echo "Domain - " . $newDomain . "</br>";;
+	echo "Can be visited at http://" . $newDomain . "</br>";;
+	echo "File Transfer Protocol - FTP Details" . "</br>";;
+	echo "FTP Username - " . $newUsername . "</br>";;
+	echo "FTP Password - " . $password_ftp . "</br>";;
+	echo "FTP Host - " . $newDomain . "</br>";;
 	echo '</br>';
-	echo "File Transfer Protocol - FTP Details";
+	echo "Database Details </br>";
+	echo "Database Username - " . $newUsername . "</br>";;
+	echo "Database Password - " . $password_db . "</br>";;
+	echo "Database Host - " . $newDomain . "</br>";;
+	echo "Database Name - " . $newDb . "</br>";;
 	echo '</br>';
-	echo "FTP Username - " . $newUsername;
-	echo '</br>';
-	echo "FTP Password - " . $password_ftp;
-	echo '</br>';
-	echo "FTP Host - " . $newDomain;
-	echo '</br>';
-	echo "Database Details";
-	echo "Database Username - " . $newUsername;
-	echo '</br>';
-	echo "Database Password - " . $password_db;
-	echo '</br>';
-	echo "Database Host - " . $newDomain;
-	echo '</br>';
-	echo "Database Name - " . $newDb;
-	echo '</br>';
-	echo "Email Details";
-	echo '</br>';
-	echo "Email Username - " . $newUsername . '@' . $newDomain;
-	echo '</br>';
-	echo "Email Password - " . $password_email;
-	echo '</br>';
-	echo "Webmail location - " . $newDomain;
+	echo "Email Details </br>";
+	echo "Email Username - " . $newUsername . '@' . $newDomain . "</br>";;
+	echo "Email Password - " . $password_email . "</br>";;
+	echo "Webmail location - " . $newDomain . "</br>";;
 
 	
 	
@@ -271,9 +373,4 @@ try {
 	echo $client->__getLastResponse();
 	die('SOAP Error: '.$e->getMessage());
 }
-	
-
-
-
-
 	
